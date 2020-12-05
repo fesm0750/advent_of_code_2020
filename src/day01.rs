afb55fc918240ad34cc065046044c2add08cc5fa
Fernando Martins <emmanuel.martins@outlook.com>
2020-12-05 05:10:59 -0300

    Refactor day01 code
    Changed how the code is organised inside the module:
    
    - The underline algorithms to solve the puzzles became their own
    functions (two_sum and three_sum);
    
    - The functionality for solving the puzzles is now inside run(). This also triggered a change in the main file;
    
    - More test cases and now they use lazy_static for shared input.

--- src/day01.rs
+++ src/day01.rs
@@ -2,11 +2,14 @@
 //!
 //! ## Problem:
 //! From an input file containig unsigned integer values:
-//! 1. find the two entries that sum to 2020 and multiply those numbers together;
+//! 1. find two entries that sum to 2020 and multiply those numbers together;
 //! 2. find three entries that sum to 2020 and multiply them together.
 
+use crate::helpers;
+
 /// Parses a string containing the input data and returns a sorted array.
-/// input should be a list of values separated by the new line character. If a value is negative or cannot be parsed, a value of zero is assigned.
+/// input should be a list of values separated by the new line character. If an
+/// entry is negative or cannot be parsed, a value of zero is assigned.
 pub fn parse_input(input: &str) -> Vec<u32> {
     let mut out: Vec<u32> = input
         .lines()
@@ -16,18 +19,18 @@ pub fn parse_input(input: &str) -> Vec<u32> {
     out
 }
 
-/// Tries to find a pair that sums to target by the two-pointer technique (left and right cursors).
-/// If values are found, it returns their multiplication. Otherwise, returns None.
+/// Tries to find a pair that sums to a target by the two-pointer technique
+/// (left and right cursors).
 /// ## Assumptions
-/// the input array must be sorted.
-pub fn puzzle01a(sorted: &[u32], target: u32) -> Option<u32> {
+/// input array is sorted.
+fn two_sum(sorted: &[u32], target: u32) -> Option<(u32, u32)> {
     let mut left = 0;
     let mut right = sorted.len() - 1;
 
     while right > left {
         let sum = sorted[left] + sorted[right];
         if sum == target {
-            return Some(sorted[left] * sorted[right]);
+            return Some((sorted[left], sorted[right]));
         }
         if sum > target {
             right -= 1;
@@ -39,40 +42,68 @@ pub fn puzzle01a(sorted: &[u32], target: u32) -> Option<u32> {
     return None;
 }
 
-/// Tries to find a triplet that sums to target integer value.
-/// If values are found, it returns their multiplication. Otherwise, returns None.
+/// Tries to find a triplet that sums to a target integer value.
 /// ## Assumptions
-/// the input array must be sorted.
-pub fn puzzle01b(sorted: &[u32], target: u32) -> Option<u32> {
-    for v in sorted {
-        if *v > target {
+/// input array is sorted.
+fn three_sum(sorted: &[u32], target: u32) -> Option<(u32, u32, u32)> {
+    for &pivot in sorted {
+        if pivot > target {
             break;
         }
-        if let Some(x) = puzzle01a(sorted, target - v) {
-            return Some(v * x);
+        if let Some((a, b)) = two_sum(sorted, target - pivot) {
+            return Some((pivot, a, b));
         }
     }
     return None;
 }
 
+//--------------------------------------------------------------------
+// Solution
+//--------------------------------------------------------------------
+
+pub fn run() {
+    let str = &helpers::read_to_str("day01").expect("Unable to read file.");
+    let sorted = parse_input(str);
+    // part 01
+    let (a, b) = two_sum(&sorted, 2020).expect("No pair of values found.");
+    // part 02
+    let (c, d, e) = three_sum(&sorted, 2020).expect("No triplet found.");
+    println!("Day 01");
+    println!("Pair: {} * {} = {}", a, b, a * b);
+    println!("Triplet: {} * {} * {} = {}", c, d, e, c * d * e);
+    println!();
+}
+
+//--------------------------------------------------------------------
+// Tests
+//--------------------------------------------------------------------
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_puzzle01a() {
-        let list = vec![299, 366, 675, 979, 1456, 1721];
-        assert_eq!(puzzle01a(&list, 2020).unwrap(), 514579);
+    use lazy_static::lazy_static;
+
+    lazy_static! {
+        static ref INPUT: Vec<u32> = vec![299, 366, 675, 979, 1456, 1721];
     }
 
     #[test]
-    fn test_puzzle01a_2() {
-        let list = vec![299, 366, 675, 979, 1456, 1721];
-        assert_eq!(puzzle01a(&list, 1041).unwrap(), 247050);
+    fn test_parser() {
+        let input = "1721\n979\n366\n299\n675\n1456";
+        assert_eq!(parse_input(input), *INPUT);
     }
 
     #[test]
-    fn test_puzzle01b() {
-        let list = vec![299, 366, 675, 979, 1456, 1721];
-        assert_eq!(puzzle01b(&list, 2020).unwrap(), 241861950);
+    fn test_two_sum() {
+        assert_eq!(two_sum(INPUT.as_ref(), 1041), Some((366, 675)));
+        assert_eq!(two_sum(INPUT.as_ref(), 1653), None);
+        assert_eq!(two_sum(INPUT.as_ref(), 1654), Some((675, 979)));
+        assert_eq!(two_sum(INPUT.as_ref(), 1655), None);
+        assert_eq!(two_sum(INPUT.as_ref(), 2020), Some((299, 1721)));
+    }
+
+    #[test]
+    fn test_three_sum() {
+        assert_eq!(three_sum(INPUT.as_ref(), 1041), None);
+        assert_eq!(three_sum(INPUT.as_ref(), 2020), Some((366, 675, 979)));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 use adv20::day01;
-use adv20::helpers;
 
 fn main() {
-    let s1 = helpers::read_to_str("day01").unwrap();
-    let in1 = day01::parse_input(&s1);
-    let v1 = day01::puzzle01a(&in1, 2020).unwrap();
-    println!("{}", v1); // 902451
-    let v2 = day01::puzzle01b(&in1, 2020).unwrap();
-    println!("{}", v2);
+    day01::run();
 }
