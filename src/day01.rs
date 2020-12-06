e2583dbedb5746aaad03260733094512472f5010
Fernando Martins <emmanuel.martins@outlook.com>
2020-12-06 14:32:06 -0300

    Refactor `two_sum()` for performance and readability
    `two_sum()` is now written in a more functional style and uses iterators
    instead of array indexing. The function is also filtering values larger
    than `target` before processing. Documentation has been improved and
    bench tests added.
    
    Using iterators improve performance because they do not do bound
    checking like array indexing. Filtering is also pretty helpful if
    `target` is a small value.

--- src/day01.rs
+++ src/day01.rs
@@ -5,7 +5,9 @@
 //! 1. find two entries that sum to 2020 and multiply those numbers together;
 //! 2. find three entries that sum to 2020 and multiply them together.
 
+extern crate test;
 use crate::helpers;
+use std::cmp::Ordering;
 
 /// Parses a string containing the input data and returns a sorted array.
 /// input should be a list of values separated by the new line character. If an
@@ -19,27 +21,38 @@ pub fn parse_input(input: &str) -> Vec<u32> {
     out
 }
 
-/// Tries to find a pair that sums to a target by the two-pointer technique
-/// (left and right cursors).
-/// ## Assumptions
-/// input array is sorted.
-fn two_sum(sorted: &[u32], target: u32) -> Option<(u32, u32)> {
-    let mut left = 0;
-    let mut right = sorted.len() - 1;
+/// Tries to find a pair that sums to a target.
+///
+/// # Arguments
+///
+/// - `sorted`  - A **sorted** array slice.
+/// - `target`  - The sum we are looking for.
+///
+/// # Assumptions
+///
+/// The input array slice `sorted` is sorted and does not contain zeros.
+///
+/// # Implementation Details
+///
+/// Uses the two pointer technique (low and high cursors) implemented directly
+/// on the iterator output values to achive O(n) performance.
+///
+/// todo: explore alternatives using Dynamic Programming, HashSets and
+/// HashMaps.
+pub fn two_sum(sorted: &[u32], target: u32) -> Option<(u32, u32)> {
+    // filter is nice if target is a small value
+    let mut iter = sorted.iter().filter(|&&x| x < target);
+    let mut lo = *iter.next()?;
+    let mut hi = *iter.next_back()?;
 
-    while right > left {
-        let sum = sorted[left] + sorted[right];
-        if sum == target {
-            return Some((sorted[left], sorted[right]));
-        }
-        if sum > target {
-            right -= 1;
-        }
-        if sum < target {
-            left += 1;
+    loop {
+        let sum = lo + hi;
+        match sum.cmp(&target) {
+            Ordering::Less => lo = *iter.next()?,
+            Ordering::Greater => hi = *iter.next_back()?,
+            Ordering::Equal => return Some((lo, hi)),
         }
     }
-    return None;
 }
 
 /// Tries to find a triplet that sums to a target integer value.
@@ -81,14 +94,17 @@ pub fn run() {
 mod tests {
     use super::*;
     use lazy_static::lazy_static;
+    use test::Bencher;
 
     lazy_static! {
         static ref INPUT: Vec<u32> = vec![299, 366, 675, 979, 1456, 1721];
+        static ref PARSED: Vec<u32> =
+            parse_input(&helpers::read_to_str("day01").expect("Unable to read file."));
     }
 
     #[test]
     fn test_parser() {
-        let input = "1721\n979\n366\n299\n675\n1456";
+        let input = "1721\n979\n366\n299\n675\n1456\n282";
         assert_eq!(parse_input(input), *INPUT);
     }
 
@@ -105,5 +121,25 @@ mod tests {
     fn test_three_sum() {
         assert_eq!(three_sum(INPUT.as_ref(), 1041), None);
         assert_eq!(three_sum(INPUT.as_ref(), 2020), Some((366, 675, 979)));
+    }
+    //--------------------------------------------------------------------
+    // Benches
+    //--------------------------------------------------------------------
+
+    // For the Advent of Code Target
+    #[bench]
+    fn bench_two_sum(b: &mut Bencher) {
+        b.iter(|| two_sum(PARSED.as_ref(), 2020));
+    }
+
+    #[bench]
+    fn bench_three_sum(b: &mut Bencher) {
+        b.iter(|| three_sum(PARSED.as_ref(), 2020));
+    }
+
+    // Large target and no matches
+    #[bench]
+    fn bench_three_sum2(b: &mut Bencher) {
+        b.iter(|| three_sum(PARSED.as_ref(), 1711));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+#![feature(test)]
 pub mod day01;
 pub mod helpers;
