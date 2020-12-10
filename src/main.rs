3596bbf3d9903c197e570a95240d777c874d5f07
Fernando Martins <emmanuel.martins@outlook.com>
2020-12-10 05:28:23 -0300

    Fix no early return
    The code now early returns if no parameter has been passed by the
    command line.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use std::env;
 fn main() {
     let input = env::args().nth(1);
     if input.is_none() {
-        println!("No input argument.")
+        println!("No input argument.");
+        return;
     }
+
     match input.unwrap().parse().unwrap() {
         1 => day01::run(),
         2 => day02::run(),
